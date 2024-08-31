d30f18eee75f6a6c6bf2d71fab155348d9e89c68
Pablo Bernal Alarcón <bononov6@yahoo.com>
2024-08-31 09:49:47 -0500

    Added a new wheel and edited game manager
    the game has a simple scoring system

--- Assets/Pablo/P_Scripts/P_GameManager.cs
+++ Assets/Pablo/P_Scripts/P_GameManager.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class P_GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Static instance of GameManager which allows it to be accessed by any other script.
+    public static P_GameManager Instance { get; private set; }
+
+    [SerializeField]
+    P_UIController UIController;
+
+    // Called when the script instance is being loaded
+    private void Awake()
     {
-        
+        // Check if the instance already exists and destroy this one if it does
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // Set the instance to this object and make it persistent across scenes
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Example of a variable or a game state
+    public float playerScore = 0;
+
+    private void Update()
     {
-        
+        playerScore += Time.deltaTime;
+        UIController.scoreText.text = ((int)playerScore).ToString();
     }
+
+
 }
