285d2f8bdfb2dc2d18ea7f83c1b63e41b9e3b437
Donuttective <Donuttective@gmail.com>
2024-08-31 11:00:01 -0500

    Se logro
    Background movement

--- Assets/Tonio/A_Back ground/A_ParalaxEffect.cs
+++ Assets/Tonio/A_Back ground/A_ParalaxEffect.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ParalaxEffect : MonoBehaviour
+public class A_ParalaxEffect : MonoBehaviour
 {
     private float length, starpos;
     public GameObject cam;
@@ -9,12 +9,13 @@ public class ParalaxEffect : MonoBehaviour
     {
         starpos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
+        float speed = parallaxEfect;
 
     }
 
     
     void FixedUpdate()
-    {
+    {   
         float temp = (cam.transform.position.x * (1 - parallaxEfect));
         float dist = (cam.transform.position.x * parallaxEfect);
         transform.position = new Vector3(starpos + dist, transform.position.y, transform.position.z);
